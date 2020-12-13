67ec69d6829fc371e6888eca403b975a112e97d9
jinbkim <jinbkim@student.42seoul.kr>
2020-12-13 19:52:08 +0900

    dijkstra

--- dijkstra/baek_1261.cpp
+++ dijkstra/baek_1261.cpp
@@ -1,77 +1,51 @@
-// #include <iostream>
-// #include <queue>
-// #include <algorithm>
-// using namespace std;
+#include <iostream>
+#include <cstring>
+using namespace std;
 
-// #define	MAX 102
-// #define	INF	987654321
+char	map[102][102];
+int		dir[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+int		dp[102][102];
+int		m, n;
 
-// vector < pair<int, int> >	vec[MAX][MAX];
-// string	map[MAX];
-// long long	min_d[MAX][MAX];
-// int		n, m;
+void	init(void)
+{
+	memset(dp, -1, sizeof(dp));
+}
 
-// void	init(void)
-// {
-// 	for(int i=1; i<=n; i++)
-// 		for(int j=1; j<=m; j++)
-// 			min_d[i][j] = INF;
-// 	min_d[1][1] = 0;
-// }
+void	dfs(int y, int x, int c)
+{
+	int		next_y, next_x;
 
-// int		dijkstra(int n, int m)
-// {
-// 	priority_queue< pair<int, int> >	pq;
-// 	int	x1, y1, x2, y2;
+	if (dp[y][x] != -1 && dp[y][x] <= c)
+		return ;
+	// cout<<"y : "<<y<<", x : "<<x<<", c : "<<c<<'\n';
+	dp[y][x] = c;	
+	for(int i=0; i<4; i++)
+	{
+		next_x = dir[i][0]+x;
+		next_y = dir[i][1]+y;
+		if (1<=next_x && next_x<=m && 1<=next_y && next_y<=n)
+		{
+			if (map[next_y][next_x] == '0')
+				dfs(next_y, next_x, c);
+			else if (map[next_y][next_x]=='1' && (dir[i][0]==1||dir[i][1]==1))
+				dfs(next_y, next_x, c+1);
+		}
+	}
+}
 
-// 	pq.push(make_pair(1, 1));
-// 	while(!pq.empty())
-// 	{
-// 		cout<<"pq size : "<<pq.size()<<'\n';
-// 		x1 = pq.top().second;
-// 		y1 = pq.top().first;
-// 		pq.pop();
-// 		for(int i=0; i<vec[y1][x1].size(); i++)
-// 		{
-// 			x2 = vec[y1][x1][i].second;
-// 			y2 = vec[y1][x1][i].first;
-// 			if (min_d[y1][x1]+1 < min_d[y2][x2])
-// 			{
-// 				min_d[y2][x2] = min_d[y1][x1]+1;
-// 				cout<<"x1 : "<<x1<<", y1 : "<<y1<<'\n';
-// 				cout<<"min_d[y1][x1] : "<<min_d[y1][x1]<<'\n';
-// 				cout<<"x2 : "<<x2<<", y2 : "<<y2<<'\n';
-// 				cout<<"min_d[y2][x2] : "<<min_d[y2][x2]<<'\n';
-// 				pq.push(make_pair(y2, x2));
-// 			}
-// 		}
-// 	}
-// 	return (min(min_d[n][m-1], min_d[n-1][m]));
-// }
 
-// int		main(void)
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(NULL);
-// 	cout.tie(NULL);
+int		main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-// 	cin>>n>>m;
-// 	for(int i=1; i<=n; i++)
-// 		for(int j=1; j<=m; j++)
-// 			cin>>map[i][j];
-
-// 	for(int i=1; i<=n; i++)
-// 		for(int j=1; j<=m; j++)
-// 		{
-// 			if (i != 1 && map[i-1][j] == '1')
-// 				vec[i][j].push_back(make_pair(i-1, j));
-// 			if (i != n && map[i+1][j] == '1')
-// 				vec[i][j].push_back(make_pair(i+1, j));
-// 			if (j != 1 && map[i][j-1] == '1')
-// 				vec[i][j].push_back(make_pair(i, j-1));
-// 			if (j != m && map[i][j+1] == '1')
-// 				vec[i][j].push_back(make_pair(i, j+1));
-// 		}
-// 	init();
-// 	cout<<dijkstra(n, m)<<'\n';
-// }
+	cin>>m>>n;
+	for(int i=1; i<=n; i++)
+		for(int j=1; j<=m; j++)
+			cin>>map[i][j];
+	init();
+	dfs(1, 1, 0);
+	cout<<dp[n][m]<<'\n';
+}
--- dijkstra/test.cpp
+++ dijkstra/test.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <queue>
 using namespace std;
 
-vector< pair<int, int> >	vec[1002];
-int		dis[1002];
-int		n, m, from, to, weight;
+char	map[102][102];
+int		dis[102][102];
+int		dir[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+int		m, n;
 
-void	dijkstra(int start)
+void	dijkstra(void)
 {
-	priority_queue< pair<int, int> >	pq;
-	int	v, w, next_v, next_w;
+	priority_queue< pair<int, pair<int, int> > >	pq;
+	int	w, x, y, next_w, next_x, next_y;
 
-	pq.push(make_pair(0, start));
+	pq.push(make_pair(0, make_pair(1, 1)));
 	while (!pq.empty())
 	{
-		v = pq.top().second;
-		w = -pq.top().first;
+		x = pq.top().second.second;
+		y = pq.top().second.first;
+		w = -pq.top().first;  // 최대힙을 최소힙처럼 사용하기위해
 		pq.pop();
-		if (dis[v] < w)
+		if (dis[y][x] < w)
 			continue ;
-		for(int i=0; i<vec[v].size(); i++)
+		for(int i=0; i<4; i++)
 		{
-			next_v = vec[v][i].second;
-			next_w = vec[v][i].first+w;
-			if (next_w < dis[next_v])
+			next_x = x+dir[i][0];
+			next_y = y+dir[i][1];
+			if (1<=next_x && next_x<=m && 1<=next_y && next_y<=n)
 			{
-				dis[next_v] = next_w;
-				pq.push(make_pair(-next_w, next_v));
+				next_w = w+map[next_y][next_x]-'0';
+				if (next_w < dis[next_y][next_x])
+				{
+					dis[next_y][next_x] = next_w;
+					// 최대힙을 최소힙처럼 사용하기위해
+					pq.push(make_pair(-next_w, make_pair(next_y, next_x)));
+				}
 			}
 		}
 	}
-
 }
-
-
 
 int		main(void)
 {
@@ -42,15 +47,17 @@ int		main(void)
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin>>n>>m;
-	for(int i=0; i<m; i++)
-	{
-		cin>>from>>to>>weight;
-		vec[from].push_back(make_pair(weight, to));
-	}
-	cin>>from>>to;
+	cin>>m>>n;
 	for(int i=1; i<=n; i++)
-		dis[i] = 987654321;
-	dijkstra(from);
-	cout<<dis[to]<<'\n';
+		for(int j=1; j<=m; j++)
+		{
+			cin>>map[i][j];
+			dis[i][j] = 987654321;
+		}
+	dijkstra();
+	// 입력에 (1,1)과 (n,m)이 뚤려있다고 가정했는데 그렇지 않은 입력값이 들어오는것 같음
+	if (dis[n][m] == 987654321)
+		cout<<0<<'\n';
+	else
+		cout<<dis[n][m]<<'\n';
 }
